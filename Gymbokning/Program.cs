30191deff76b31355419a5d01b213fdeaa8340ea
RakanEngineer <rakan.engineer@gmail.com>
2026-01-14 03:15:30 +0100

    Add Identity UI and GymClass CRUD scaffolding - Switched Identity to use ApplicationUser and enabled roles - Added login, registration, and profile management Razor Pages - Implemented GymClassesController - Added views for GymClass management

--- Gymbokning/Program.cs
+++ Gymbokning/Program.cs
@@ -1,4 +1,5 @@
 using Gymbokning.Data;
+using Gymbokning.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,8 @@ namespace Gymbokning
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
